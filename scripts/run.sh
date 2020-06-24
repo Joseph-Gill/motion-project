d726190128197532ddf1488639be76bd10bb3f31
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-06-24 16:13:41 +0200

    Deploy works, fixing issue with Gunicorn

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python manage.py collectstatic --no-input
-gunicorn -w 4 -b 0.0.0.0:9000 backend_project.wsgi:application
+gunicorn -w 4 -b 0.0.0.0:8000 backend_project.wsgi:application
