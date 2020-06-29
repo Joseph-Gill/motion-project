da7b1a5a7a6d7ff3eb1495af934e2570d963b74e
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-06-29 18:20:01 +0200

    Fixed issues in PostImages model.

--- backend/postimages/models.py
+++ backend/postimages/models.py
@@ -3,16 +3,17 @@ from posts.models import Post
 
 
 class PostImage(models.Model):
-    class Meta:
+    post = models.ForeignKey(
+        to=Post,
+        related_name='images',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
 
-        post = models.ForeignKey(
-            to=Post,
-            related_name='images',
-            on_delete=models.CASCADE,
-            null=True,
-            blank=True,
-        )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
 
-        created = models.DateTimeField(
-            auto_now_add=True,
-        )
+    def __str__(self):
+        return f'Image {self.pk}: for Post {self.post}'
