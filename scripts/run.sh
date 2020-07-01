8691311a0c74990795df48fe641f3fe1d21227fb
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-01 18:37:53 +0200

    Fixing issues with DB migration on Server

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations --no-input
 python manage.py migrate
 gunicorn -w 4 -b 0.0.0.0:8000 backend_project.wsgi:application
