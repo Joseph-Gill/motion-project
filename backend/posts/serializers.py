2822a1760fed13b1cc1be807a3d00aa1eb175e4a
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-01 16:58:37 +0200

    Fixing issues with DB migration on Server

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-
-from comments.serializers import CommentSerializer
 from postimages.models import PostImage
 from postimages.serializers import PostImageSerializer
 from posts.models import Post
